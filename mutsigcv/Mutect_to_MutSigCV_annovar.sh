6b54b37736e2ab43d9ef6dd4c143b4f50bf71ebb
Hiuyu <drizzle2013@foxmail.com>
2016-06-22 17:07:10 +0800

    update vcf to table

--- mutsigcv/Mutect_to_MutSigCV_annovar.sh
+++ mutsigcv/Mutect_to_MutSigCV_annovar.sh
@@ -10,27 +10,34 @@ module load java/1.8.0_60
 
 VCF_PATH="/data/home/zuoxy/data/NPC/somatic/20160519_863closing/4-driver_gene"
 IDS_LIST=$1 # please input the full path of the IDS file.
+REGION="/data/home/zuoxy/resource/DNASeq/hg19/Captured-regions_Agilent_HG19_AllExonV5.bed" #only include variants in these regions for mutsig analysis
 ##############################################
 # 1. annotate VCF by using annovar
 ##############################################
 pass_dir="./1-pass_mutect2"
 mutsig_dir="./3-mutsigcv_annotate"
+mutsig_temp_dir="./3-mutsigcv_annotate/temp"
 annovar_dir="./2-annovar_output"
-COMBINE_MAF="$mutsig_dir/combine.maf"
+COMBINE_MAF="$mutsig_dir/combine_annovar.maf"
+
 mkdir -p $pass_dir
-#mkdir -p $mutsig_dir
+mkdir -p $mutsig_dir
+mkdir -p $mutsig_temp_dir
 mkdir -p $annovar_dir
 HAS_HEADER="no"
 for sample in `cat ${IDS_LIST}`;
 do
-  INPUT_VCF=$VCF_PATH/1-mutect2_output/${sample}_mutect2_out_g.vcf
-  PASS_VCF=$pass_dir/${sample}_pass_mutect2.vcf
-  ANV_OUT=$annovar_dir/$sample
+  INPUT_VCF="$VCF_PATH/1-mutect2_output/${sample}_mutect2_out_g.vcf"
+  PASS_VCF="$pass_dir/${sample}_pass_mutect2.vcf"
+  ANV_OUT="$annovar_dir/$sample"
+  TARGET_TABLE="$mutsig_temp_dir/${sample}.target.txt"
   #####################################
   # filtering mutect2 raw output
+  # restricted to targeted region
   #####################################
-  CMD="vcftools --vcf $INPUT_VCF --remove-filtered-all  --recode-INFO-all --recode --stdout"
-#  CMD="java -jar $HOME/apps/GenomeAnalysisTK/3.4.0/GenomeAnalysisTK.jar -T SelectVariants -R $HOME/resource/DNASeq/hg19/ucsc_hg19.fa -V $INPUT_VCF -o $PASS_VCF -ef "
+  CMD="vcftools --vcf $INPUT_VCF  --remove-filtered-all  --recode-INFO-all --recode --stdout"
+## seems to slow and give up 
+## CMD="java -jar $HOME/apps/GenomeAnalysisTK/3.4.0/GenomeAnalysisTK.jar -T SelectVariants -R $HOME/resource/DNASeq/hg19/ucsc_hg19.fa -V $INPUT_VCF -o $PASS_VCF -ef "
   echo "filtering mutect2 for $sample"
   echo $CMD
   #time $CMD > $PASS_VCF
@@ -42,20 +49,40 @@ do
   CMD="perl /data/home/zuoxy/apps/annovar/2015Dec14/table_annovar.pl $PASS_VCF /data/home/zuoxy/resource/annovar_hg19/humandb/ -buildver hg19 -out $ANV_OUT -remove -protocol refGene,avsnp144,cosmic74,dbnsfp30a,clinvar_20150629,1000g2015aug_all,1000g2015aug_afr,1000g2015aug_eas,1000g2015aug_eur,1000g2015aug_amr,1000g2015aug_sas,exac03,esp6500siv2_all,dbnsfp31a_interpro,dbscsnv11,kaviar_20150923,hrcr1 -operation g,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f --onetranscript -nastring . -vcfinput --dot2underline -thread 4"
   echo "annotating $sample"
   echo "[running command]: $CMD"
+  #time $CMD
+  #########################################
+  # vcf back to table, restricted to target regions !  
+  #########################################
+  CMD="java -jar $HOME/apps/GenomeAnalysisTK/3.5.0/GenomeAnalysisTK.jar -T VariantsToTable -R $HOME/resource/DNASeq/hg19/ucsc_hg19.fa -V ${ANV_OUT}.hg19_multianno.vcf --allowMissingData --showFiltered -o ${TARGET_TABLE} -F CHROM -F POS -F REF -F ALT -F Gene_refGene -F Func_refGene -F avsnp144 -F ExonicFunc_refGene -F AAChange_refGene -F Kaviar_AC -F Kaviar_AN -F Kaviar_AF -F HRC_AC -F HRC_AN -F HRC_AF -F 1000g2015aug_all -F ExAC_ALL -F SIFT_pred -F SIFT_score -F PROVEAN_pred -F PROVEAN_score -GF GT -GF AD -GF DP -GF BQ  -GF FA -AMD -L ${REGION}"
+  echo "vcf back to the table for $sample"
+  echo "[running command]: $CMD"
   time $CMD
-  if [ $HAS_HEADER == 'yes' ]
+  #########################################
+  # recode variant type and prepare for 
+  # mutsigcv input
+  #########################################
+  if [ $HAS_HEADER == 'no' ]
   then
     # because the vcf2maf.pl would write the first line as a comment line
-    cat $OUTPUT_MAF|head -n 2|tail -n 1 > $COMBINE_MAF
-    HAS_HEADER="no"
+    #perl $prepare_maf_script_pl $sample > $COMBINE_MAF
+    HAS_HEADER="yes"
+  else
+    #perl $prepare_maf_script_pl $sample|perl -lane 'print if $. >1' >> $COMBINE_MAF
   fi
-  #cat $OUTPUT_MAF|perl -lane 'print if $.>2' >> $COMBINE_MAF
 done
+
+##############################################
+# do some filter, 
+# e.g. population frequency,
+#      sequencing coverage(depth),
+#      mutant allele frequency in tumor and normal
+##############################################
+
+
 
 ##############################################
 # calling MutSigCV
 ##############################################
-exit
 CMD="$HOME/apps/MATLAB/r2013a/bin/matlab -nodisplay < mutsig_perform.m "
 echo "calling MutSigCV on combine maf"
 echo "[running command]: $CMD"
